ef2487f2e11bb090cd8fe4f85a99b0cd5c3311fb
averak <abe12@mccc.jp>
2020-08-10 05:35:30 +0900

    [add] nameオブジェクトを追加

--- src/models/elements/id.py
+++ src/models/elements/id.py
@@ -2,13 +2,13 @@
 
 
 class ID:
-    def __init__(self, id):
-        if id is None:
+    def __init__(self, id_):
+        if id_ is None:
             raise Exception('idがNoneです')
-        if id < 0 or type(id) is not int:
+        if id_ < 0 or type(id_) is not int:
             raise Exception('idは0以上の整数を指定してください')
 
-        self.__id = id
+        self.__id = id_
 
     def get_id(self):
         return self.__id
--- src/tests/test_model_elements.py
+++ src/tests/test_model_elements.py
@@ -9,3 +9,7 @@ class TestModelElements(unittest.TestCase):
     def test_id_create(self):
         id_ = elements.ID(0)
         self.assertEqual(0, id_.get_id())
+
+    def test_name_create(self):
+        name = elements.Name('競走馬名')
+        self.assertEqual('競走馬名', name.get_name())
